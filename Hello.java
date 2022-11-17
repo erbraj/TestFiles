db7245808a39e7546c0da858217089080751ccf3
erbraj <79088886+erbraj@users.noreply.github.com>
2022-11-17 17:03:43 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
   public static void main(String args[])
     {
-    System.out.println("*********Welcome back , it has been long time********");
+    System.out.println("*********Welcome back time********");
     }
 }
