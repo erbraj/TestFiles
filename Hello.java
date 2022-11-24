94bfd77c09ea617fbaa1bb6da36a3d21edea82c0
erbraj <79088886+erbraj@users.noreply.github.com>
2022-11-24 21:34:55 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello{
   public static void main(String args[])
     {
-    System.out.println("Hello Technical team,Bangalore");
+    int n1 = 225, n2 = 115, sum;  
+    sum = n1 + n2;  
+    System.out.println("The addition is :"+sum);
     }
 }
