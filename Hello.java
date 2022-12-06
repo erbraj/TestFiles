e6cc563c43b3464a8c84ad0d60378cb5272e48c9
Brajendra <79088886+erbraj@users.noreply.github.com>
2022-12-06 13:37:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
   public static void main(String args[])
     {
-    int n1 = 225, n2 = 285, sum;  
+    int n1 = 225, n2 = 500, sum;  
     sum = n1 + n2;  
     System.out.println("Addition is :-"+sum);
     }
